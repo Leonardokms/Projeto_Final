d928422b5a4997ccda740245e060b36970c9d6fc
Igor Sprovieri <igsp1@hotmail.com>
2021-12-02 11:20:14 -0300

    player

---
+++ Assets/Scripts/MonoBehaviour/PlayerAtacar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAtacar : MonoBehaviour
+{
+    public Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "Inimigo")
+            {
+                Vector2 PosInimigo = hit.transform.position;
+                Vector2 PosPlayer = transform.position;
+                float dis = Vector2.Distance(PosInimigo, PosPlayer);
+
+                if (Input.GetKey("A"))
+                {
+                    if (dis < 0.5f)
+                    {
+                        hit.transform.GetComponent<InimigoBase>().ReceberDano(1);
+                    }
+                }
+            }
+        }
+    }
+
+
+}
